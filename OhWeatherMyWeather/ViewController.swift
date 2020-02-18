cb8e0941a29b199a0574bc0cebcd7e7833e83c1f
jakob salomonsson <jakob@salomonssons.se>
2020-02-18 14:37:43 +0100

    Initial Commit

---
+++ OhWeatherMyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OhWeatherMyWeather
+//
+//  Created by Jakob Salomonsson on 2020-02-18.
+//  Copyright © 2020 Jakob Salomonsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
